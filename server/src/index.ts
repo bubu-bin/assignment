3a981226b90b94ecab7c8506d33d3b03f6fb2b57
cajetan-gd-soft <cajetan.kasprzyk@gdsoft.info>
2023-05-25 15:35:38 +0200

    Assignment-2: Packages Install packages for the client and server

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,9 +1,14 @@
 import express from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = express();
 
+const { PORT } = process.env;
+
 app.get('/', (req, res) => {});
 
-app.listen(8000, () => {
-  console.log(`Now listening on port 8000`);
+app.listen(PORT, () => {
+  console.log(`Now listening on port ${PORT}`);
 });
